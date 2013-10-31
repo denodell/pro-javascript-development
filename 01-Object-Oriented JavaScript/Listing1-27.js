ea4cc2c89d1b053b881f78b9e39c7261f72f593b
Dennis Odell <Dennis.Odell@akqa.com>
2013-10-31 10:04:00 +0100

    Add comment

--- 01-Object-Oriented JavaScript/Listing1-27.js
+++ 01-Object-Oriented JavaScript/Listing1-27.js
@@ -1,5 +1,6 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+// The forEach method allows you to loop through each item in an array, executing a function each time
 months.forEach(function(value, index, fullArray) {
     alert(value + " is month number " + (index + 1) + " of " + fullArray.length);
 });
