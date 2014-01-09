09eb5b4f580dcfb84c99646f4e7f339f327d399d
Den Odell <Den.Odell@akqa.com>
2014-01-09 20:44:26 +0100

    Updated code listings

--- 01-Object-Oriented JavaScript/Listing1-19.js
+++ 01-Object-Oriented JavaScript/Listing1-19.js
@@ -1,15 +1,15 @@
-// Define an object called Class with a ‘create’ method for use creating "classes".
+// Define an object called Class with a create() method for use creating "classes".
 // Use a closure to maintain inner functions without exposing them publicly.
 var Class = (function() {
 
-    // The ‘create’ method defines and returns a new "class" when called, based on an object
+    // The create() method defines and returns a new "class" when called, based on an object
     // literal representing the public properties and methods for its prototype. A method named
-    // ‘initialize’ will be executed as the constructor function. If an optional
+    // initialize() will be executed as the constructor function. If an optional
     // ‘parentPrototype’ property is passed in, representing a parent "class", it creates the
-    // new class as a subclass of that.
+    // new "class" as a subclass of that.
     function create(classDefinition, parentPrototype) {
 
-        // Define the constructor function of a new "class", using the ‘initialize’ method from
+        // Define the constructor function of a new "class", using the initialize() method from
         // the ‘classDefinition’ object literal if it exists
         var _NewClass = function() {
                 if (this.initialize && typeof this.initialize === 'function') {
@@ -30,6 +30,23 @@ var Class = (function() {
             }
         }
 
+        // Define a function to create a closure and return a function to replace the one
+        // passed in, wrapping it and providing a __parent() method which points to the
+        // method of the same name from a parent "class", to enable support for polymorphism
+        function polymorph(thisFunction, parentFunction) {
+            return function () {
+                var output;
+
+                this.__parent = parentFunction;
+
+                output = thisFunction.apply(this, arguments);
+
+                delete this.__parent;
+
+                return output;
+            };
+        }
+
         // Apply the newly provided "class" definition, overriding anything that already exists
         // from the parentPrototype
         for (_name in classDefinition) {
@@ -40,29 +57,7 @@ var Class = (function() {
                 // parent function of the same name in a simple way
                 if (parentPrototype && parentPrototype[_name] &&
                         typeof classDefinition[_name] === 'function') {
-
-                    // We create a closure and return a function to replace the one passed in,
-                    // wrapping it and providing a ‘__parent()’ method which points to the
-                    // method of the same name from the parent "class"
-                    _NewClass.prototype[_name] = (function(thisFunction, parentFunction) {
-                        return function() {
-                            var output;
-
-                            // Expose the parent "class" function of the same name as a
-                            // method named ‘__parent’
-                            this.__parent = parentFunction;
-
-                            // execute the function we’re replacing – allowing that function
-                            // to access the ‘_parent’ method if it so wishes
-                            output = thisFunction.apply(this, arguments);
-
-                            // Remove the ‘__parent’ method now the function has executed
-                            delete this.__parent;
-
-                            // Return the output of the original function
-                            return output;
-                        };
-                    }(classDefinition[_name], parentPrototype[_name]));
+                    _NewClass.prototype[_name] = polymorph(classDefinition[_name], parentPrototype[_name]);
                 } else {
 
                     // If we’re not attempting polymorphism, just map over the entry from the
@@ -76,7 +71,7 @@ var Class = (function() {
         // ‘constructor’ property or method was passed in the ‘classDefinition’ object literal)
         _NewClass.prototype.constructor = _NewClass;
 
-        // Define an ‘extend’ method on the "class" itself, pointing to the private ‘extend’
+        // Define an extend() method on the "class" itself, pointing to the private extend()
         // function, below, which allows the current "class" to be used as a parent for
         // a new subclass
         _NewClass.extend = extend;
@@ -84,13 +79,13 @@ var Class = (function() {
         return _NewClass;
     }
 
-    // The ‘extend’ method is the same as the ‘create’ method but with an additional parameter
+    // The extend() method is the same as the create() method but with an additional parameter
     // containing the prototype from the parent "class" for inheriting from
     function extend(classDefinition) {
         return create(classDefinition, this.prototype);
     }
 
-    // Expose the private ‘create’ method publicly under the same name
+    // Expose the private create() method publicly under the same name
     return {
         create: create
     };
--- 05-Using Design Patterns/Listing5-34.js
+++ 05-Using Design Patterns/Listing5-34.js
@@ -1,6 +1,14 @@
+// Define two mediators for our code base, one pertaining to code for a forms feature, and
+// another to enable a message logging feature.
+// The formsMediator will feature two events: "form-submit", and "ajax-response", whereas
+// the loggingMediator will feature three events, "log", "retrieve-log", and "log-retrieved".
+// Note how we're able to separate events for different features in our code using the
+// mediator pattern
 var formsMediator = new Mediator(),
     loggingMediator = new Mediator();
 
+// Define a module for Ajax communication which POSTs some supplied data to the server when a
+// "form-submit" event is triggered within the formsMediator
 (function(formsMediator) {
     function ajaxPost(url, data, callback) {
         var xhr = new XMLHttpRequest();
@@ -27,8 +35,11 @@ var formsMediator = new Mediator(),
     });
 }(formsMediator));
 
+// Define a module for handling submission of a simple form on the page containing text fields
+// only with an ID of "my-form". When the form is submitted, the "form-submit" event is
+// triggered within the formsMediator
 (function(formsMediator) {
-    var form = document.getElementById("mailinglist-form"),
+    var form = document.getElementById("my-form"),
         action = form.action,
         data = [],
         fields = form.getElementsByTagName("input"),
@@ -57,24 +68,46 @@ var formsMediator = new Mediator(),
     });
 }(formsMediator));
 
+// Define a module for logging messages within the system to aid with debugging of issues that
+// might occur. Uses the loggingMediator to separate the logging feature of the code base
+// separate from that handling the form submission with the formsMediator
 (function(loggingMediator) {
+
+    // Create an array to store the logs
     var logs = [];
 
-    loggingMediator.subscribe("log", function(trackingText) {
+    // When the "log" event is triggered on the loggingMediator, add an object to the logs
+    // containing a supplied message and the date / time that the message was received at
+    loggingMediator.subscribe("log", function(message) {
         logs.push({
-            message: trackingText,
+            message: message,
             date: new Date()
         });
     });
 
+    // When the "retrieve-log" event is triggered on the loggingMediator, trigger the
+    // "log-retrieved" event, passing along the current state of the stored logs
     loggingMediator.subscribe("retrieve-log", function() {
         loggingMediator.publish("log-retrieved", logs);
     });
 }(loggingMediator));
 
-(function(formsMediator, loggingMediator) {
+// Define a module which allows the stored logs in the loggingMediator to be displayed on screen
+(function(loggingMediator) {
+
+    // Create a button which, when clicked, will display the current state of the log
     var button = document.createElement("button");
 
+    button.innerHTML = "Show logs";
+
+    button.addEventListener("click", function() {
+
+        // Trigger the "retrieve-log" event within the loggingMediator. This triggers the
+        // "log-retrieved" event, passing along the current state of the logs
+        loggingMediator.publish("retrieve-log");
+    }, false);
+
+    // When the "log-retrieved" event occurs, display the logs on screen
     loggingMediator.subscribe("log-retrieved", function(logs) {
         var index = 0,
             length = logs.length,
@@ -82,28 +115,36 @@ var formsMediator = new Mediator(),
             liTag = document.createElement("li"),
             listItem;
 
+        // Loop through each log in the list of logs, rendering the date / time and message
+        // stored within a <li> tag
         for (; index < length; index++) {
             listItem = liTag.cloneNode(false);
             listItem.innerHTML = logs[index].date.toUTCString() + ": " + logs[index].message;
             ulTag.appendChild(listItem);
         }
 
+        // Add the <ul> tag containing all the <li> tags representing the log data to the bottom
+        // of the page
         document.body.appendChild(ulTag);
     });
 
+    // Add the button to the bottom of the current page
+    document.body.appendChild(button);
+}(loggingMediator));
+
+// Define a module which logs events that occur within the formsMediator. This is the only
+// module in this example to use more than one mediator
+(function(formsMediator, loggingMediator) {
+
+    // Use the loggingMediator's "log" events to log the URL the form is submitted to when the
+    // "form-submit" event is triggered within the formsMediator
     formsMediator.subscribe("form-submit", function(url) {
         loggingMediator.publish("log", "Form submitted to " + url);
     });
 
+    // Log the response from the server that is supplied when the "ajax-response" event is
+    // triggered within the formsMediator
     formsMediator.subscribe("ajax-response", function(response) {
         loggingMediator.publish("log", "The server responded to an Ajax call with: " + response);
     });
-
-    button.innerHTML = "Show logs";
-
-    button.addEventListener("click", function() {
-        loggingMediator.publish("retrieve-log");
-    }, false);
-
-    document.body.appendChild(button);
 }(formsMediator, loggingMediator));
